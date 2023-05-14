b178bba58afa3a42897207103eaafc67357fb578
Moreyad <91405739+reyad-7@users.noreply.github.com>
2023-05-14 17:24:03 +0300

    student , main class  added
    only contain our student data

---
+++ student.cpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Student {
+    int id;
+    string fname;
+    string lname;
+    float gpa;
+    string department;
+    Student(){}
+
+    Student(int i, string fn, string ln , float g, string d) : id(i), fname(fn), lname(ln) ,gpa(g), department(d) {}
+
+    void print (){
+        cout <<"["<< id << " , " << fname <<lname << " , " << gpa << " , " << department <<"]"<<"\n";
+    }
+
+};
